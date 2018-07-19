dfeaf2de9fb38e1ce3b5f1ba6bdf28ecc76a7f42
Barkov_Evgenii <barkovevgenii@inbox.ru>
2018-07-19 18:08:11 +0300

    Unsuccessful 10 Hz blinking

--- work/main.c
+++ work/main.c
@@ -1,29 +1,42 @@
 /* Заголовочный файл для нашего семейства микроконтроллеров*/
 #include "stm32f3xx.h"
 
+#define F_CPU 		    12000000UL
+#define BlinkFreq       10
+#define TimerTick  	    (F_CPU / (2 * BlinkFreq) - 1)    // N - requiers ticks (N <= 2^24)
+                                                        // N - 1 - should be written
+
 const int LED[] = {0x200, 0x100, 0x400, 0x8000, 0x800, 0x4000, 0x1000, 0x2000};
+int LEDs = 0;
 
 /* Тело основной программы */
 int main(void)
 {
 	/* Включаем тактирование на порту GPIO */
 	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
-	
-	/* Настраиваем режим работы портов PC8 и PC9 в Output*/
+	/* Настраиваем режим работы портов LED в Output*/
 	GPIOE->MODER = 0x55550000;
 	/* Настраиваем Output type в режим Push-Pull */
 	GPIOE->OTYPER = 0;
-	
 	/* Настраиваем скорость работы порта в Low */
 	GPIOE->OSPEEDR = 0;
-	
-    short LED_index = 0;
+ 
+    SysTick->LOAD = TimerTick;		    // Загрузка значения
+    SysTick->VAL = TimerTick;		    // Обнуляем таймеры и флаги
+ 
+    SysTick->CTRL =	SysTick_CTRL_CLKSOURCE_Msk |
+                    SysTick_CTRL_TICKINT_Msk   |
+                    SysTick_CTRL_ENABLE_Msk;
     
-	while(1)
-	{
-		/* Поочередное зажигание диодов */
-		GPIOE->ODR = LED[LED_index];
-		for (int i = 0; i < 500000; ++i) {}	// Искусственная задержка
-        LED_index = LED_index == (sizeof(LED)/sizeof(int) - 1) ? 0 : LED_index + 1;
-	}		
+    for (int i = 0; i < sizeof(LED) / sizeof(int); ++i) {
+        LEDs |= LED[i];
+    }
+    
+    GPIOE->ODR = LEDs;
+}
+
+/* Попеременно зажигает светодиоды по прерыванию */
+void SysTick_Handler(void)
+{  
+    GPIOE->ODR ^= LEDs;
 }
