89f7fa5e0686995e845963e23281a23643fff5d1
Barkov_Evgenii <barkovevgenii@inbox.ru>
2018-07-20 16:49:00 +0300

    LED_init(), LED_on() and LED_off() are included

--- work/main.c
+++ work/main.c
@@ -3,27 +3,45 @@
 
 const int LED[] = {0x200, 0x100, 0x400, 0x8000, 0x800, 0x4000, 0x1000, 0x2000};
 const int CircleLEDind[] = {0, 1, 3, 5, 7, 6, 4, 2};
-/* Тело основной программы */
-int main(void)
+
+/* Инициализация портов для LED */
+void LED_init()
 {
-	/* Включаем тактирование на порту GPIO */
+    /* Включаем тактирование на порту GPIO */
 	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
-	
-	/* Настраиваем режим работы портов PC8 и PC9 в Output*/
+	/* Настраиваем режим работы портов связанных с LED в Output*/
 	GPIOE->MODER = 0x55550000;
 	/* Настраиваем Output type в режим Push-Pull */
 	GPIOE->OTYPER = 0;
-	
 	/* Настраиваем скорость работы порта в Low */
 	GPIOE->OSPEEDR = 0;
+}
+
+/* Зажечь диод LED[index] */
+void LED_on(short index)
+{
+    GPIOE->ODR |= LED[index];
+}
+
+/* Погасить диод LED[index] */
+void LED_off(short index)
+{
+    GPIOE->ODR &= ~LED[index];
+}
+
+/* Тело основной программы */
+int main(void)
+{
+	LED_init();
 
     short index = 0;
     
 	while(1)
 	{
 		/* Поочередное зажигание диодов */
-		GPIOE->ODR = LED[CircleLEDind[index]];
+		LED_on(index);
 		for (int i = 0; i < 500000; ++i) {}	// Искусственная задержка
-        	index = index == (sizeof(LED)/sizeof(int) - 1) ? 0 : index + 1;
+        LED_off(index);
+        index = index == (sizeof(LED)/sizeof(int) - 1) ? 0 : index + 1;
 	}		
 }
